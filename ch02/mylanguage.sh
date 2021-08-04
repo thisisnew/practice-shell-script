ec6a01320f105c59a8ddd208e5dfbda7dd3effbc
thisisnew <thisisnew@naver.com>
2021-08-04 21:55:40 +0900

    2장 쉘 스크립트 기초 문법 공부

---
+++ ch02/mylanguage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "This shell script name is $0"
+echo "I can speak $1 and $2"
+echo "This shell script parameters are $*"
+echo "This shell script parameters are $@"
+echo "This parameter count is $#"
+
