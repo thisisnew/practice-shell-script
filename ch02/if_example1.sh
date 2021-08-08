503e7ac70794acb49592d138ee54a40d1d1a76a6
thisisnew <thisisnew@naver.com>
2021-08-08 21:22:32 +0900

    2장 셸 스크립트 기초문법 공부

---
+++ ch02/if_example1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+value1=10
+value2=10
+
+
+if [ $value1 = $value2 ]
+then
+echo True
+else
+echo False
+fi
---
+++ ch02/if_example2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+value=0
+
+if [ $value = 0 ]; then
+	echo True; else
+	echo False;fi
+
